81ff4c2f0fdf78eda554c31bb82fe915720a1281
Obrero <hapcosa@gmail.com>
2024-11-26 23:09:51 -0300

    fix cors origin deploy

--- core/settings.py
+++ core/settings.py
@@ -343,10 +343,16 @@ DEFAULT_FROM_EMAIL = 'Piedras rayadito - Taller de joyeria y lapidacion <piedras
     #}
 
 CORS_ALLOWED_ORIGINS=['http://localhost:5173',
-                      'http://127.0.0.1:5173',
+                      'http://127.0.0.1:5173','https://rayaditoecomerce-production.up.railway.app',
+                      'https://7bdd-191-127-236-145.ngrok-free.app'
+                      ,'http://127.0.0.1:8000'
                       ]
 CORS_ORIGIN_WHITELIST = ['http://localhost:5173',
-                      'http://127.0.0.1:5173',
+                      'http://127.0.0.1:5173','https://rayaditoecomerce-production.up.railway.app',
+                      'https://7bdd-191-127-236-145.ngrok-free.app'
+                      ,'http://127.0.0.1:8000'
                       ]
 CSRF_TRUSTED_ORIGINS = ['http://localhost:5173',
-                      'http://127.0.0.1:5173',]
+                      'http://127.0.0.1:5173','https://rayaditoecomerce-production.up.railway.app',
+                      'https://7bdd-191-127-236-145.ngrok-free.app'
+                      ,'http://127.0.0.1:8000']
